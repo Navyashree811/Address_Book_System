874d509d99076fc21f6d67e580ca12fc7cf329bf
hp-pc <navyashree811@gmail.com>
2022-08-12 22:20:42 +0530

    [NAVAYA]ADDED : Create a Contacts class

--- src/com/bridgelabz/addressbook/WelcomeAddressBook.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.addressbook;
-
-public class WelcomeAddressBook {  
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book System");
-	}
-
-}
