14f495099f5801924ffcf24ec616fc0d9c17eac8
hp-pc <navyashree811@gmail.com>
2022-08-31 23:45:38 +0530

    Added : To get number of contact by City or State

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,89 +1,16 @@
 package com.bridgelabz.addressbook;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class AddressBook {
-	static List<ContactsDetails> contactList = new ArrayList<>();
-	static Scanner input = new Scanner(System.in);
+	private List<ContactPerson> contacts;
 
-	// method for adding contacts
-	public static void addContact() {
-		System.out.println(" Enter your first name : ");
-		String firstName = input.nextLine();
-		// No duplicate entries Allow
-		for (int index = 0; index < contactList.size(); index++) {
-			if (firstName.equals(contactList.get(index).getfirstName())) {
-				System.out.println(" The name already exists ");
-				AddressBookMain.entryOptions();
-			}
-		}
-		System.out.println(" Enter your last name : ");
-		String lastName = input.nextLine();
-		System.out.println(" Enter your city name : ");
-		String city = input.nextLine();
-		System.out.println("Enter your state  : ");
-		String state = input.nextLine();
-		System.out.println(" Enter your zip code : ");
-		long zip = input.nextLong();
-		System.out.println(" Enter your phone number : ");
-		long phoneNumber = input.nextLong();
-		System.out.println(" Enter your email : ");
-		String email = input.nextLine();
-		ContactsDetails addressBook = new ContactsDetails(firstName, lastName, email, city, state, phoneNumber, zip);
-		contactList.add(addressBook);
-
+	public List<ContactPerson> getContacts() {
+		return contacts;
 	}
 
-// method for editing existing contact
-	public void editContact() {
-		Scanner nameInput = new Scanner(System.in);
-		System.out.println(" Enter the first name ");
-		String fName = nameInput.nextLine();
-		for (int index = 0; index < contactList.size(); index++) {
-			if (contactList.get(index).getfirstName().equals(fName)) {
-				contactList.remove(index);
-				AddressBook addressBook = new AddressBook();
-				addressBook.addContact();
-			} else {
-				System.out.println(" There is no contact ");
-			}
-		}
+	public void setContacts(List<ContactPerson> contacts) {
+		this.contacts = contacts;
 	}
 
-	public void deleteContact() {
-		Scanner deleteNameInput = new Scanner(System.in);
-		String deleteFirstName = deleteNameInput.nextLine();
-		for (int increment = 0; increment < contactList.size(); increment++) {
-			if (contactList.get(increment).getfirstName().equals(deleteFirstName)) {
-				contactList.remove(increment);
-			} else {
-				System.out.println(" Name does not exist");
-			}
-		}
-	}
-
-	// Search a person
-	public void searchByCity() {
-		Scanner cityInput = new Scanner(System.in);
-		System.out.println(" Enter the city name you want to get contact details ");
-		String city = cityInput.nextLine();
-		for (ContactsDetails cityList : contactList) {
-			if (cityList.getCity().equals(city))
-				System.out.println(cityList);
-		}
-
-	}
-
-	// View a person by his city
-	public void viewPersonByCity() {
-		Scanner cityViewInput = new Scanner(System.in);
-		System.out.println(" Enter the city name you want to get the person details");
-		String city = cityViewInput.nextLine();
-		for (ContactsDetails cityList : contactList) {
-			if (cityList.getCity().equals(city))
-				System.out.println(cityList);
-		}
-	}
 }
